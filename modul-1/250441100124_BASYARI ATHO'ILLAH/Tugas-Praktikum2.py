f6679ad0bc9ab22b59416ac612e336f507f708b8
IRAYYY <basyaribelajar@gmail.com>
2025-10-01 13:15:13 +0700

    250441100124_BASYARI ATHO'ILLAH_MODUL1_Kak Angga

---
+++ modul-1/250441100124_BASYARI ATHO'ILLAH/Tugas-Praktikum2.py
@@ -0,0 +1,8 @@
+#volume dan luas balok soal nomor 2
+panjang = 10
+lebar = 6
+tinggi = 4
+volume = panjang*lebar*tinggi
+luas = 2 * (panjang*lebar + panjang*tinggi + lebar*tinggi)
+print("Hasil volume balok tersebut adalah", volume, "cm")
+print("Hasil luas balok tersebut adalah", luas, "cm")
---
+++ modul-1/250441100124_BASYARI ATHO'ILLAH/Tugas-Praktikum3.py
@@ -0,0 +1,7 @@
+merah = 8
+biru = 6
+total = merah + biru
+ambil = 3
+
+kemungkinan = (total * (total - 1) * (total - 2)) / (ambil * (ambil - 1) * (ambil - 2))
+print("Banyaknya kemungkinan kombinasi bola yang dapat diambil adalah ",kemungkinan)
