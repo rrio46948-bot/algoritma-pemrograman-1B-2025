9118f45be066780644c4ddc4c4fe31e28ab29713
vinandamarbun01 <vinandakristin290@gmail.com>
2025-10-02 09:49:46 +0700

    250441100112_VinandaKristianiMarbun_Modul1_AspKakAngga

---
+++ modul-1/250441100112_Vinanda Kristiani Marbun/soal1.py
@@ -0,0 +1,7 @@
+# menghitung total belanja setelah pajak di tetapkan
+
+buku = 3 * 5000
+pensil = 2 * 4500
+total = buku + pensil
+pajak = total * 0.10
+print(total + pajak)
---
+++ modul-1/250441100112_Vinanda Kristiani Marbun/soal3.py
@@ -0,0 +1,14 @@
+merah = 8
+biru = 6
+total = merah + biru
+
+bola_diambil = 3
+
+faktorial_total = 14*13*12*11*10*9*8*7*6*5*4*3*2*1
+
+faktorial_ambil = 3*2*1
+
+faktorial_sisa = 11*10*9*8*7*6*5*4*3*2*1
+
+jumlah_kombinasi = faktorial_total // (faktorial_ambil * faktorial_sisa)
+print("Jumlah kemungkinan kombinasi pengambilan", bola_diambil,"bola dari", total, "bola adalah:", jumlah_kombinasi)
