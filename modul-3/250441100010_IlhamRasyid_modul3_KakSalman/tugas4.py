58ffd04d722d1b74a35ec1453e1af34d3c737fe7
Ilham Rasyid <ilhampretenk01@gmail.com>
2025-10-16 14:54:11 +0700

    250441100010_IlhamRasyid_Modul-3_KakSalman

---
+++ modul-3/250441100010_IlhamRasyid_modul3_KakSalman/tugas4.py
@@ -0,0 +1,22 @@
+while True:
+    nama = input("Masukkan nama pembeli: ")
+    jumlah_barang = int(input("Masukkan jumlah barang yang dibeli: "))
+    
+    total = 0
+    daftar_barang = "" 
+    
+    for i in range(jumlah_barang):
+        nama_barang = input(f"Nama barang ke {i+1}: ")
+        harga = int(input(f"Harga {nama_barang}: "))
+        total += harga
+        daftar_barang += f"{nama_barang} - Rp{harga}"  
+    
+    print("Nama pembeli:", nama)
+    print("Daftar barang:")
+    print(daftar_barang, end="") 
+    print("Total harga: Rp", total)
+    print("Terima kasih telah berbelanja di IndoMei.")
+
+    lanjut = input("Apakah ada pembeli berikutnya? (y/n): ")
+    if lanjut.lower() != 'y':
+        break
