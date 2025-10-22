a2205abfb87e4c3c5d9962b2eda270309f2d80ca
danx3300607 <wildanfirdauswf18@gmail.com>
2025-10-22 18:02:48 +0700

    250441100014_WildanFirdaus_UTS_KakSalman

---
+++ UTS/soal-1.py
@@ -0,0 +1,19 @@
+print("rental motor AconkGG")
+
+matic= 5000000
+trail = 10000000
+sport = 7500000
+diskon = 10
+asuransi = 1500000
+Stotal = 1500000
+sewa = 3
+diskon = 5
+kupon = 'AconkGG'
+
+if sewa > 3  :
+
+else:
+    print()
+
+
+     
